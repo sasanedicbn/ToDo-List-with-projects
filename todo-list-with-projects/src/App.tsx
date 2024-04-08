22ed1ecbe3bbedcd69a3c931af02cb4d49ca0747
Sasa Nedic <svetasmak0@gmail.com>
2024-04-08 16:35:48 +0200

    Create style for SideBar component

--- todo-list-with-projects/src/App.tsx
+++ todo-list-with-projects/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <>
      <Header/>
+     <SideBar/>
     </>
   )
 }
--- todo-list-with-projects/src/components/SideBar.tsx
+++ todo-list-with-projects/src/components/SideBar.tsx
@@ -2,7 +2,7 @@ const SideBar = () => {
     return(
         <div className="sidebar">
             <button className="sidebar-btn">+ New Project</button>
-            <p className="sidebar-text">Projects ↓</p>
+            <p className="sidebar-text">Projects </p>
             <ul></ul>
         </div>
     )
