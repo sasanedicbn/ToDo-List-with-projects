bad23e79b30bf0ea215280134e80265fa7472cd7
Sasa Nedic <svetasmak0@gmail.com>
2024-04-08 12:50:52 +0200

    Create header component

---
+++ todo-list-with-projects/src/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return(
+        <div className="header-container">
+            <p className="header-title">TODO LIST</p>
+        </div>
+    )
+}
+export default Header;
