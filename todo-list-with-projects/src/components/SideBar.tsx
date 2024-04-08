f43279088de03b9a6e66d7714dc9aecfff87015a
Sasa Nedic <svetasmak0@gmail.com>
2024-04-08 13:39:09 +0200

    Create SideBar compoenent

---
+++ todo-list-with-projects/src/components/SideBar.tsx
@@ -0,0 +1,10 @@
+const SideBar = () => {
+    return(
+        <div className="sidebar">
+            <button className="sidebar-btn">+ New Project</button>
+            <p className="sidebar-text">Projects ↓</p>
+            <ul></ul>
+        </div>
+    )
+}
+export default SideBar;
