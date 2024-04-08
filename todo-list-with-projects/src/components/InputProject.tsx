da1a0e1a5276c533b11431d7ff0d7d40e6ecbbf7
Sasa Nedic <svetasmak0@gmail.com>
2024-04-09 00:12:59 +0200

    Create classes

--- todo-list-with-projects/src/components/InputProject.tsx
+++ todo-list-with-projects/src/components/InputProject.tsx
@@ -1,12 +1,15 @@
-const InputProject = ({removeInputhandler}) => {
-    return(
-        <li>
-          <div>
-            <input type='text'/>
-            <button>Add</button>
-            <button onClick={removeInputhandler}>Cancel</button>
+
+
+const InputProject = ({ removeInputhandler }) => {
+    return (
+        <li className="input-project-item">
+            <div className="input-project-container"> 
+                <input type='text' className="input-field" />
+                <button className="add-button">Add</button> 
+                <button onClick={removeInputhandler} className="cancel-button">Cancel</button> 
             </div>
         </li>
-    )
+    );
 }
+
 export default InputProject;
