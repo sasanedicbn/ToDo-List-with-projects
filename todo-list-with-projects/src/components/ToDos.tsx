3abd2ae6d8db2807bf87d868282eef30a6fe1bde
Sasa Nedic <svetasmak0@gmail.com>
2024-04-12 20:45:18 +0200

    Create ToDos component

--- todo-list-with-projects/src/components/ToDos.tsx
+++ todo-list-with-projects/src/components/ToDos.tsx
@@ -1,8 +1,22 @@
-const ToDos = () => {
-    return(
-        <li className="input-project-item">
-       <p>SASASASAPARAGRAFFFFF</p>
+const ToDos = ({ todo }) => {
+  return (
+      <li className="input-project-item">
+          <div className="todo-details">
+              <div className="todo-title">
+                  <p>Title:</p>
+                  <p>{todo.title}</p>
+              </div>
+              <div className="todo-due-date">
+                  <p>Due date:</p>
+                  <p>{todo.dueDate}</p>
+              </div>
+              <div className="todo-actions">
+                  <button>Edit</button>
+                  <button>Delete</button>
+              </div>
+          </div>
       </li>
-    )
-}
+  );
+};
+
 export default ToDos;
