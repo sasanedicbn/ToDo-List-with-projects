c16f9b702ed09cf7ef0d5ba780a6bacf239daea6
Sasa Nedic <svetasmak0@gmail.com>
2024-04-15 15:26:03 +0200

    Create state and display inputs

--- todo-list-with-projects/src/components/ToDos.tsx
+++ todo-list-with-projects/src/components/ToDos.tsx
@@ -1,29 +1,68 @@
+import { useState } from "react";
 import { Todo } from "./TypeScript";
 
-const ToDos = ({ todo, handleDeleteToDo }:{todo: Todo,handleDeleteToDo: (id: string) => void }) => {
-  console.log(todo)
+const ToDos = ({ todo, handleDeleteToDo, handleEditToDo }: { todo: Todo, handleDeleteToDo: (id: string) => void, handleEditToDo: (id: string, updatedTodo: Todo) => void }) => {
+    const [editedTodo, setEditedTodo] = useState({ ...todo });
+    const [isEditing, setIsEditing] = useState(false);
 
-  return (
-      <li className="custom-todo-item">
-          <div className="custom-todo-details">
-              <div className="custom-todo-title">
-                  <p>Title:</p>
-                  <p>{todo.title}</p>
-              </div>
-              <div className="custom-todo-due-date">
-                  <p>Due date:</p>
-                  <p>{todo.date}</p>
-              </div>
-              <div className="custom-todo-actions">
-                  <button>Edit</button>
-                  <button onClick={() => handleDeleteToDo(todo.id)}>Delete</button>
-                 <input className="checkBox" type="checkBox" name="checkBox"/>
-              </div>
-             
-          </div>
-      </li>
-  );
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEditedTodo({
+            ...editedTodo,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSave = () => {
+        handleEditToDo(todo.id, editedTodo);
+        setIsEditing(false);
+    };
+
+    return (
+        <li className="custom-todo-item">
+            <div className="custom-todo-details">
+                <div className="custom-todo-title">
+                    {isEditing ? (
+                        <>
+                            <p>Title:</p>
+                            <input type="text" name="title" value={editedTodo.title} onChange={handleChange} />
+                        </>
+                    ) : (
+                        <>
+                            <p>Title:</p>
+                            <p>{todo.title}</p>
+                        </>
+                    )}
+                </div>
+                <div className="custom-todo-due-date">
+                    {isEditing ? (
+                        <>
+                            <p>Due date:</p>
+                            <input type="date" name="date" value={editedTodo.date} onChange={handleChange} />
+                        </>
+                    ) : (
+                        <>
+                            <p>Due date:</p>
+                            <p>{todo.date}</p>
+                        </>
+                    )}
+                </div>
+                <div className="custom-todo-actions">
+                    {isEditing ? (
+                        <>
+                            <button onClick={handleSave}>Save</button>
+                            <button onClick={() => setIsEditing(false)}>Cancel</button>
+                        </>
+                    ) : (
+                        <>
+                            <button onClick={() => setIsEditing(true)}>Edit</button>
+                            <button onClick={() => handleDeleteToDo(todo.id)}>Delete</button>
+                        </>
+                    )}
+                    <input className="checkBox" type="checkBox" name="checkBox" />
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default ToDos;
-
