1211a624652493ef4ab4669b8f6f535d1e7c062c
Sasa Nedic <svetasmak0@gmail.com>
2024-04-16 23:10:48 +0200

    Fix error

--- todo-list-with-projects/src/components/Main.tsx
+++ todo-list-with-projects/src/components/Main.tsx
@@ -5,14 +5,18 @@ import { Project, Todo } from "./TypeScript";
 
 const Main = () => {
     const [projects, setProjects] = useState<Project>([]);
-    const [activeProjectId, setActiveProjectId] = useState<string | null>(null);
+    const [activeProjectId, setActiveProjectId] = useState<string | null>(null); 
 
 
-
-};
-setProjects(prevProjects => [...prevProjects, newProject]);
+    function addProject(name:string) {
+        const newProject = {
+            id: Math.random().toString(36).substring(7),
+            name: name,
+            todos: [],
+        };
+        setProjects(prevProjects => [...prevProjects, newProject]);
     }
-function addToDo(todo: Todo, projectId: string) {
+    function addToDo(todo: Todo, projectId:string) {
     setProjects(prevProjects => {
         return prevProjects.map(project => {
             if (project.id === projectId) {
@@ -22,42 +26,42 @@ function addToDo(todo: Todo, projectId: string) {
                     todos: [...project.todos, todo]
                 };
             }
-
+           
             return project;
         });
     });
 }
-function deleteProject(id: string) {
-    setProjects(prevProjects => prevProjects.filter(project => project.id !== id));
-}
+    function deleteProject(id:string) {
+        setProjects(prevProjects => prevProjects.filter(project => project.id !== id));
+    }
 
-function showToDo(id: string) {
-    setActiveProjectId(id);
-}
-
-function deleteToDo(id: string) {
-    setProjects(prevProjects => {
-        return prevProjects.map(project => {
-            if (project.id === activeProjectId) {
-                return {
-                    ...project,
-                    todos: project.todos.filter(todo => todo.id !== id)
+    function showToDo(id:string) {
+        setActiveProjectId(id); 
+    }
+    
+    function deleteToDo(id:string) {
+        setProjects(prevProjects => {
+            return prevProjects.map(project => {
+                if(project.id === activeProjectId){
+                    return{
+                        ...project,
+                        todos: project.todos.filter(todo => todo.id !== id) 
+                    }
                 }
-            }
-            return project;
+                return project; 
+            })
         })
-    })
-}
+    }
 
+    
+    console.log(projects)
 
-console.log(projects)
-
-return (
-    <div className="container-main">
-        <SideBar projects={projects} addProject={addProject} deleteProject={deleteProject} showToDo={showToDo} />
-        <Projects projects={projects} activeProjectId={activeProjectId} addToDo={addToDo} deleteToDo={deleteToDo} setProjects={setProjects} />
-    </div>
-);
+    return(
+        <div className="container-main">
+            <SideBar projects={projects} addProject={addProject} deleteProject={deleteProject} showToDo={showToDo}/>
+            <Projects projects={projects} activeProjectId={activeProjectId} addToDo={addToDo} deleteToDo={deleteToDo} setProjects={setProjects}/> 
+        </div>
+    );
 }
 
 export default Main;
